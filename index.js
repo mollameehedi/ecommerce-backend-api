6868bcc4c616bd2d16bff60d8ff8166840416ae2
molla mehedi <mollameehedi@gmail.com>
2024-08-20 11:06:29 +0600

    db and env setup

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const dbConnection = require('./config/dbConfig')
+
+dbConnection()
+app.use(express.json)
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
-app.listen(3000,function(){
+app.listen(8000,function(){
     console.log('Server Is Running');
     
 })
